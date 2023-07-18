71d24a0cf5406ed68c5858c2d3da705177e5762a
SaraAbabneh <sara.ababneh1997@gmail.com>
2023-07-18 16:17:29 -0700

    JS_Tasks

--- task17-july-2023/task.js
+++ task17-july-2023/task.js
@@ -1,45 +1,40 @@
+function getdata() {
+  const x = new XMLHttpRequest();
+  x.open("GET", "task.json");
+  x.onload = function () {
+    const obj = JSON.parse(x.responseText);
+    console.log(obj);
 
+    let t = document.getElementById("table");
+    for (let i = 0; i < obj.length; i++) {
+      let tr = document.createElement("tr");
+      t.appendChild(tr);
 
-   function getdata(){
-    const x=new XMLHttpRequest();
-    x.open("GET","tasks.json");
-    x.onload=function (){
-    const obj=JSON.parse(this.responseText);
-    
-        
-        let t =document.getElementById("table")
-        for (let i=0 ; i<obj.length ; i++){
-        let tr =document.createElement("tr")
-        t.appendChild(tr)
+      let td1 = document.createElement("td");
+      tr.appendChild(td1);
+      td1.textContent = obj[i].name;
 
-        let td1 =document.createElement("td")
-        tr.appendChild(td1)
-        td1 .textContent = obj[i].name
+      let td2 = document.createElement("td");
+      tr.appendChild(td2);
+      td2.textContent = obj[i].Age;
 
-        let td2 =document.createElement("td")
-        tr.appendChild(td2)
-        td2 .textContent = obj[i].Age
+      let td3 = document.createElement("td");
+      tr.appendChild(td3);
+      td3.textContent = obj[i].Mjor;
 
-        let td3 =document.createElement("td")
-        tr.appendChild(td3)
-        td3 .textContent = obj[i].Mjor
+      let td4 = document.createElement("td");
+      tr.appendChild(td4);
+      td4.textContent = obj[i].University;
 
-        let td4 =document.createElement("td")
-        tr.appendChild(td4)
-        td4 .textContent = obj[i].University
+      let td5 = document.createElement("td");
+      tr.appendChild(td5);
 
-        let td5 =document.createElement("td")
-        tr.appendChild(td5)
+      let im = document.createElement("img");
+      im.src = obj[i].image;
 
-        let im =document.createElement("img")
-        im.src = obj[i].image
-
-        td5.appendChild(im)
-        im.style= "width:100px"
-
+      td5.appendChild(im);
+      im.style = "width:100px";
     }
+  };
+  x.send();
 }
-x.send();
-}
-
-
